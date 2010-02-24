932bd13919e50e32ef98fb758fa0a66ecb1f687c
Ingo Renner <ingo@typo3.org>
2010-02-24 15:29:13 +0000

    minor whitespace optimization
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tika/trunk@30443 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,63 +5,57 @@ if (!defined('TYPO3_MODE')) {
 
 $PATH_tika = t3lib_extMgm::extPath($_EXTKEY);
 
-t3lib_extMgm::addService($_EXTKEY, 'metaExtract', 'tx_tika_metaExtract',
-	array(
-		'title'       => 'Tika meta data extraction',
-		'description' => 'Uses Apache Tika to extract meta data',
+t3lib_extMgm::addService($_EXTKEY, 'metaExtract', 'tx_tika_metaExtract', array(
+	'title'       => 'Tika meta data extraction',
+	'description' => 'Uses Apache Tika to extract meta data',
 
-		'subtype'     => 'bmp,gif,image:exif,jpg,jpeg,png,svg,tiff',
+	'subtype'     => 'bmp,gif,image:exif,jpg,jpeg,png,svg,tiff',
 
-		'available'   => TRUE,
-		'priority'    => 50,
-		'quality'     => 50,
+	'available'   => TRUE,
+	'priority'    => 50,
+	'quality'     => 50,
 
-		'os'          => '',
-		'exec'        => 'java',
+	'os'          => '',
+	'exec'        => 'java',
 
-		'classFile'   => $PATH_tika . 'classes/class.tx_tika_metadataextractionservice.php',
-		'className'   => 'tx_tika_MetaDataExtractionService',
-	)
-);
+	'classFile'   => $PATH_tika . 'classes/class.tx_tika_metadataextractionservice.php',
+	'className'   => 'tx_tika_MetaDataExtractionService',
+));
 
 
-t3lib_extMgm::addService($_EXTKEY, 'textExtract', 'tx_tika_textExtract',
-	array(
-		'title'       => 'Tika text extraction',
-		'description' => 'Uses Apache Tika to extract text from files',
+t3lib_extMgm::addService($_EXTKEY, 'textExtract', 'tx_tika_textExtract', array(
+	'title'       => 'Tika text extraction',
+	'description' => 'Uses Apache Tika to extract text from files',
 
-		'subtype'     => 'doc,docx,epub,html,msg,odt,pdf,ppt,pptx,rtf,sxw,txt,xls,xlsx,xml',
+	'subtype'     => 'doc,docx,epub,html,msg,odt,pdf,ppt,pptx,rtf,sxw,txt,xls,xlsx,xml',
 
-		'available'   => TRUE,
-		'priority'    => 50,
-		'quality'     => 50,
+	'available'   => TRUE,
+	'priority'    => 50,
+	'quality'     => 50,
 
-		'os'          => '',
-		'exec'        => 'java',
+	'os'          => '',
+	'exec'        => 'java',
 
-		'classFile'   => $PATH_tika . 'classes/class.tx_tika_textextractionservice.php',
-		'className'   => 'tx_tika_TextExtractionService',
-	)
-);
+	'classFile'   => $PATH_tika . 'classes/class.tx_tika_textextractionservice.php',
+	'className'   => 'tx_tika_TextExtractionService',
+));
 
 
-t3lib_extMgm::addService($_EXTKEY, 'textLang', 'tx_tika_textLang',
-	array(
-		'title'       => 'Tika language detection',
-		'description' => 'Uses Apache Tika to detect a document\'s language',
+t3lib_extMgm::addService($_EXTKEY, 'textLang', 'tx_tika_textLang', array(
+	'title'       => 'Tika language detection',
+	'description' => 'Uses Apache Tika to detect a document\'s language',
 
-		'subtype'     => '',
+	'subtype'     => '',
 
-		'available'   => TRUE,
-		'priority'    => 50,
-		'quality'     => 50,
+	'available'   => TRUE,
+	'priority'    => 50,
+	'quality'     => 50,
 
-		'os'          => '',
-		'exec'        => 'java',
+	'os'          => '',
+	'exec'        => 'java',
 
-		'classFile'   => $PATH_tika . 'classes/class.tx_tika_languagedetectionservice.php',
-		'className'   => 'tx_tika_LanguageDetectionService',
-	)
-);
+	'classFile'   => $PATH_tika . 'classes/class.tx_tika_languagedetectionservice.php',
+	'className'   => 'tx_tika_LanguageDetectionService',
+));
 
 ?>
