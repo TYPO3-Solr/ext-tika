7c8527fb02eb37563269eaaa157f57a3777ceaca
Ingo Renner <ingo@typo3.org>
2010-10-14 10:41:40 +0000

    Added a registry entry to store whether tika is available - which is the case if it is detected to be configured properly
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tika/trunk@39124 735d13b6-9817-0410-8766-e36946ffe9aa

--- report/class.tx_tika_report_tikastatus.php
+++ report/class.tx_tika_report_tikastatus.php
@@ -91,6 +91,10 @@ class tx_tika_report_TikaStatus implements tx_reports_StatusProvider {
 			$this->hasCompleteRemoteSolrExtractingRequestHandlerConfiguration()
 		);
 
+		/* @var $registry t3lib_Registry */
+		$registry = t3lib_div::makeInstance('t3lib_Registry');
+		$registry->set('tx_tika', 'available', $isConfigured);
+
 		return $isConfigured;
 	}
 
