53f4f2bad3fa976530d236fe17c9c097a1e21311
Ingo Renner <ingo@typo3.org>
2011-04-19 15:49:02 +0000

    Fixed wrong entry in ext_autoload.php for class tx_tika_StatusCheck
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tika/trunk@46762 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,7 +1,7 @@
 <?php
 $extensionPath = t3lib_extMgm::extPath('tika');
 return array(
-	'tx_tika_statuscheck'               => $extensionPath . 'classes/class.tx_tika_checkstatus.php',
+	'tx_tika_statuscheck'               => $extensionPath . 'classes/class.tx_tika_statuscheck.php',
 
 	'tx_tika_languagedetectionservice'  => $extensionPath . 'classes/class.tx_tika_languagedetectionservice.php',
 	'tx_tika_metadataextractionservice' => $extensionPath . 'classes/class.tx_tika_metadataextractionservice.php',
