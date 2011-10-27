d8c07b9cf9595d1ff536519df42edf43432310d1
Ingo Renner <ingo@typo3.org>
2011-10-27 15:28:58 +0000

    added some small comments
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tika/trunk@53487 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -11,6 +11,7 @@ if ($tikaConfiguration['extractor'] == 'tika') {
 	$requiredExecutable = 'java';
 }
 
+	// meta data extraction service
 t3lib_extMgm::addService($_EXTKEY, 'metaExtract', 'tx_tika_metaExtract', array(
 	'title'       => 'Tika meta data extraction',
 	'description' => 'Uses Apache Tika to extract meta data.',
@@ -28,7 +29,7 @@ t3lib_extMgm::addService($_EXTKEY, 'metaExtract', 'tx_tika_metaExtract', array(
 	'className'   => 'tx_tika_MetaDataExtractionService',
 ));
 
-
+	// text extraction service
 t3lib_extMgm::addService($_EXTKEY, 'textExtract', 'tx_tika_textExtract', array(
 	'title'       => 'Tika text extraction',
 	'description' => 'Uses Apache Tika to extract text from files.',
@@ -46,7 +47,7 @@ t3lib_extMgm::addService($_EXTKEY, 'textExtract', 'tx_tika_textExtract', array(
 	'className'   => 'tx_tika_TextExtractionService',
 ));
 
-
+	// language detection service
 t3lib_extMgm::addService($_EXTKEY, 'textLang', 'tx_tika_textLang', array(
 	'title'       => 'Tika language detection',
 	'description' => 'Uses Apache Tika to detect a document\'s language.
