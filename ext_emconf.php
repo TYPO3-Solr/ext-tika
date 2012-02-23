ec3eaa9f4cdc5a3142b8762b3c4bb1b7b197b3cf
Ingo Renner <ingo@typo3.org>
2012-02-23 16:50:25 +0000

    Increased version number to 1.3.0-dev
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tika/trunk@58386 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,7 +30,7 @@ Can either use a stand alone Tika executable or Tika integrated in a Solr server
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.2.0',
+	'version' => '1.3.0-dev',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.3.0-0.0.0',
