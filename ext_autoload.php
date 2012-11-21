6dde2ac2d4caa12af1cb5e24e06578cc3980b6c6
Ingo Renner <ingo@typo3.org>
2012-11-21 11:20:13 +0100

    [TASK] Add a comment marking the service classes

--- ext_autoload.php
+++ ext_autoload.php
@@ -3,6 +3,8 @@ $extensionPath = t3lib_extMgm::extPath('tika');
 return array(
 	'tx_tika_statuscheck'               => $extensionPath . 'classes/class.tx_tika_statuscheck.php',
 
+	// services
+
 	'tx_tika_languagedetectionservice'  => $extensionPath . 'classes/class.tx_tika_languagedetectionservice.php',
 	'tx_tika_metadataextractionservice' => $extensionPath . 'classes/class.tx_tika_metadataextractionservice.php',
 	'tx_tika_textextractionservice'     => $extensionPath . 'classes/class.tx_tika_textextractionservice.php',
