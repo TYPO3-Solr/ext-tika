df6cc7793560b656a7acf0701171b010e626a999
Ingo Renner <ingo@typo3.org>
2014-05-28 23:50:50 -0700

    Use ExtensionManagementUtility instead of t3lib_extMgm

--- ext_tables.php
+++ ext_tables.php
@@ -5,7 +5,7 @@ if (!defined ('TYPO3_MODE')) {
 
 if (TYPO3_MODE == 'BE') {
 		// adding the Solr connection status to the status report
-	$statusSection = t3lib_extMgm::isLoaded('solr') ? 'solr' : 'tika';
+	$statusSection = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('solr') ? 'solr' : 'tika';
 
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers'][$statusSection][] = 'tx_tika_report_TikaStatus';
 }
