29df848866ee24afe2248121fb4242fbde39001f
Ingo Renner <ingo@typo3.org>
2014-05-31 19:13:07 -0700

    Update ext_localconf.php for TYPO3 CMS 6.0+

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-$PATH_tika = t3lib_extMgm::extPath($_EXTKEY);
+$PATH_tika = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY);
 
 $tikaConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['tika']);
 $requiredExecutable = '';
@@ -11,58 +11,71 @@ if ($tikaConfiguration['extractor'] == 'tika') {
 	$requiredExecutable = 'java';
 }
 
-	// meta data extraction service
-t3lib_extMgm::addService($_EXTKEY, 'metaExtract', 'tx_tika_metaExtract', array(
-	'title'       => 'Tika meta data extraction',
-	'description' => 'Uses Apache Tika to extract meta data.',
+// meta data extraction service
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService(
+	$_EXTKEY,
+	'metaExtract',
+	'tx_tika_metaExtract',
+	array(
+		'title' => 'Tika meta data extraction',
+		'description' => 'Uses Apache Tika to extract meta data.',
 
-	'subtype'     => 'aiff,au,bmp,doc,docx,epub,flv,gif,htm,html,image:exif,jpg,jpeg,mid,mp3,msg,odf,odt,pdf,png,ppt,pptx,rtf,svg,sxw,tgz,tiff,txt,wav,xls,xlsx,xml,zip',
+		'subtype' => 'aiff,au,bmp,doc,docx,epub,flv,gif,htm,html,image:exif,jpg,jpeg,mid,mp3,msg,odf,odt,pdf,png,ppt,pptx,rtf,svg,sxw,tgz,tiff,txt,wav,xls,xlsx,xml,zip',
 
-	'available'   => FALSE,
-	'priority'    => 100,
-	'quality'     => 80,
+		'available' => FALSE,
+		'priority' => 100,
+		'quality' => 80,
 
-	'os'          => '',
-	'exec'        => $requiredExecutable,
+		'os' => '',
+		'exec' => $requiredExecutable,
 
-	'className'   => 'ApacheSolrForTypo3\\Tika\\Service\\MetaDataExtractionService',
-));
+		'className' => 'ApacheSolrForTypo3\\Tika\\Service\\MetaDataExtractionService',
+	)
+);
 
-	// text extraction service
-t3lib_extMgm::addService($_EXTKEY, 'textExtract', 'tx_tika_textExtract', array(
-	'title'       => 'Tika text extraction',
-	'description' => 'Uses Apache Tika to extract text from files.',
+// text extraction service
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService(
+	$_EXTKEY,
+	'textExtract',
+	'tx_tika_textExtract',
+	array(
+		'title' => 'Tika text extraction',
+		'description' => 'Uses Apache Tika to extract text from files.',
 
-	'subtype'     => 'doc,docx,epub,htm,html,msg,odf,odt,pdf,ppt,pptx,rtf,sxw,tgz,txt,xls,xlsx,xml,zip',
+		'subtype' => 'doc,docx,epub,htm,html,msg,odf,odt,pdf,ppt,pptx,rtf,sxw,tgz,txt,xls,xlsx,xml,zip',
 
-	'available'   => FALSE,
-	'priority'    => 60,
-	'quality'     => 80,
+		'available' => FALSE,
+		'priority' => 60,
+		'quality' => 80,
 
-	'os'          => '',
-	'exec'        => $requiredExecutable,
+		'os' => '',
+		'exec' => $requiredExecutable,
 
-	'className'   => 'ApacheSolrForTypo3\\Tika\\Service\\TextExtractionService',
-));
+		'className' => 'ApacheSolrForTypo3\\Tika\\Service\\TextExtractionService',
+	)
+);
 
-	// language detection service
-t3lib_extMgm::addService($_EXTKEY, 'textLang', 'tx_tika_textLang', array(
-	'title'       => 'Tika language detection',
-	'description' => 'Uses Apache Tika to detect a document\'s language.
-						Currently supports Danish, Dutch, English, Finnish,
-						French, German, Italian, Portuguese, Spanish,
-						and Swedish',
+// language detection service
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService(
+	$_EXTKEY,
+	'textLang',
+	'tx_tika_textLang',
+	array(
+		'title' => 'Tika language detection',
+		'description' => 'Uses Apache Tika to detect a document\'s language.
+							Currently supports Danish, Dutch, English, Finnish,
+							French, German, Italian, Portuguese, Spanish,
+							and Swedish',
 
-	'subtype'     => '',
+		'subtype' => '',
 
-	'available'   => FALSE,
-	'priority'    => 60,
-	'quality'     => 60,
+		'available' => FALSE,
+		'priority' => 60,
+		'quality' => 60,
 
-	'os'          => '',
-	'exec'        => $requiredExecutable,
+		'os' => '',
+		'exec' => $requiredExecutable,
 
-	'className'   => 'ApacheSolrForTypo3\\Tika\\Service\\LanguageDetectionService',
-));
-
-?>
+		'className' => 'ApacheSolrForTypo3\\Tika\\Service\\LanguageDetectionService',
+	)
+);
