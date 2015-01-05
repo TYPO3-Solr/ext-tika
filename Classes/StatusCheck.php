8854393330f0805a10631de6843e3fdd08d327ac
Ingo Renner <ingo@typo3.org>
2015-01-05 10:46:55 -0800

    Happy New Year!

--- Classes/StatusCheck.php
+++ Classes/StatusCheck.php
@@ -4,7 +4,7 @@ namespace ApacheSolrForTypo3\Tika;
 /***************************************************************
 *  Copyright notice
 *
-*  (c) 2011-2014 Ingo Renner <ingo@typo3.org>
+*  (c) 2011-2015 Ingo Renner <ingo@typo3.org>
 *  All rights reserved
 *
 *  This script is part of the TYPO3 project. The TYPO3 project is
