8aa8e6a5bc91b58f541e491b09f8e90836a69dcf
Ingo Renner <ingo@typo3.org>
2015-01-13 21:40:52 -0800

    Remove old service registrations

--- ext_tables.php
+++ ext_tables.php
@@ -11,18 +11,3 @@ if (TYPO3_MODE == 'BE') {
 }
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][] = 'ApacheSolrForTypo3\\Tika\\StatusCheck->updateStatus';
-// checking availability. Must do this here, DB connection is not available yet when ext_localconf.php is loaded
-#$registry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('t3lib_Registry');
-#$servicesAvailable = $registry->get('Tx_Tika', 'available', FALSE);
-
-// Permanently enabled for now
-#TODO use registry with TYPO3 CMS 6.x, too. Must figure out earliest availability of DB
-$servicesAvailable = true;
-
-$GLOBALS['T3_SERVICES']['metaExtract']['Tx_Tika_MetaExtract']['available'] = $servicesAvailable;
-$GLOBALS['T3_SERVICES']['textExtract']['Tx_Tika_TextExtract']['available'] = $servicesAvailable;
-$GLOBALS['T3_SERVICES']['textLang']['Tx_Tika_TextLang']['available'] = $servicesAvailable;
-
-$GLOBALS['T3_SERVICES']['Tx_Tika_MetaExtract']['Tx_Tika_MetaExtract']['available'] = $servicesAvailable;
-$GLOBALS['T3_SERVICES']['Tx_Tika_TextExtract']['Tx_Tika_TextExtract']['available'] = $servicesAvailable;
-$GLOBALS['T3_SERVICES']['Tx_Tika_TextLang']['Tx_Tika_TextLang']['available'] = $servicesAvailable;
