c6eec44742bcbe6423f0cf73e18fffb03f78d4bd
Ingo Renner <ingo@typo3.org>
2015-08-10 22:17:17 -0700

    Require EXT:solr 3.0.2+

--- ext_emconf.php
+++ ext_emconf.php
@@ -38,7 +38,7 @@ Can either use a stand alone Tika executable or Tika integrated in a Solr server
 		'conflicts' => array(
 		),
 		'suggests' => array(
-			'solr' => '3.1.0-',
+			'solr' => '3.0.2-',
 			'devlog' => '',
 		),
 	),
