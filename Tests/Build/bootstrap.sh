7db644219591c7098d159b6145d2873e6bd520be
Ingo Renner <ingo@typo3.org>
2015-08-17 01:03:25 -0700

    [Travis] Add directory debugging

--- Tests/Build/bootstrap.sh
+++ Tests/Build/bootstrap.sh
@@ -22,6 +22,7 @@ echo "Starting Apache Tika"
 nohup java -jar "$TIKA_PATH/tika-server-$TIKA_VERSION.jar" > /dev/null 2>&1 &
 
 cd ..
+echo `pwd`
 
 # clone TYPO3
 git clone --single-branch --branch $TYPO3_BRANCH --depth 1 https://github.com/TYPO3/TYPO3.CMS.git typo3_core
@@ -29,6 +30,7 @@ mv typo3_core/* .
 composer self-update
 composer install --prefer-dist
 mkdir -p uploads typo3temp typo3conf/ext/tika
+echo `ls -l`
 
 # clone EXT:solr
 git clone --single-branch --branch master --depth 1 https://github.com/TYPO3-Solr/ext-solr.git solr
