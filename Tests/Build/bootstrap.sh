9e9a99f504b8cb0c9de13b9a1e484278eb0495a3
Ingo Renner <ingo@typo3.org>
2015-10-05 14:05:53 +0200

    Use composer to install TYPO3 core

--- Tests/Build/bootstrap.sh
+++ Tests/Build/bootstrap.sh
@@ -24,18 +24,27 @@ nohup java -jar "$TIKA_PATH/tika-server-$TIKA_VERSION.jar" > /dev/null 2>&1 &
 cd ..
 echo "cd to $(pwd)"
 
+
+composer require typo3/cms="$TYPO3_VERSION"
+# Restore composer.json
+git checkout composer.json
+export TYPO3_PATH_WEB=$PWD/.Build/Web
+
+
+
+
 # clone TYPO3
-git clone --single-branch --branch $TYPO3_BRANCH --depth 1 https://github.com/TYPO3/TYPO3.CMS.git typo3_core
-mv typo3_core/* .
-composer self-update
-composer install --prefer-dist
-mkdir -p uploads typo3temp typo3conf/ext/tika
+#git clone --single-branch --branch $TYPO3_BRANCH --depth 1 https://github.com/TYPO3/TYPO3.CMS.git typo3_core
+#mv typo3_core/* .
+#composer self-update
+#composer install --prefer-dist
+mkdir -p $TYPO3_PATH_WEB/uploads $TYPO3_PATH_WEB/typo3temp $TYPO3_PATH_WEB/typo3conf/ext/tika
 
 
 # clone EXT:solr
-git clone --single-branch --branch master --depth 1 https://github.com/TYPO3-Solr/ext-solr.git solr
+#git clone --single-branch --branch master --depth 1 https://github.com/TYPO3-Solr/ext-solr.git solr
 
-mv solr typo3conf/ext/
-cp -R ext-tika/* typo3conf/ext/tika/
+#mv solr typo3conf/ext/
+#cp -R ext-tika/* typo3conf/ext/tika/
 
 cd -
