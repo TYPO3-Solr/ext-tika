49b452c5ea4feccd5be7a6dfb36c40fc0bbb7316
Ingo Renner <ingo@typo3.org>
2015-10-06 13:56:32 +0200

    Remove dead code

--- Tests/Build/bootstrap.sh
+++ Tests/Build/bootstrap.sh
@@ -37,20 +37,4 @@ git checkout composer.json
 export TYPO3_PATH_WEB=$PWD/.Build/Web
 
 
-
-
-# clone TYPO3
-#git clone --single-branch --branch $TYPO3_BRANCH --depth 1 https://github.com/TYPO3/TYPO3.CMS.git typo3_core
-#mv typo3_core/* .
-#composer self-update
-#composer install --prefer-dist
 mkdir -p $TYPO3_PATH_WEB/uploads $TYPO3_PATH_WEB/typo3temp
-
-
-# clone EXT:solr
-#git clone --single-branch --branch master --depth 1 https://github.com/TYPO3-Solr/ext-solr.git solr
-
-#mv solr typo3conf/ext/
-#cp -R ext-tika/* typo3conf/ext/tika/
-
-#cd -
