53b752dea37b83a967b7adcff6792cd40ca0faaa
Ingo Renner <ingo@august.com>
2015-12-08 09:58:56 -0800

    Increase max TYPO3 version to 7.99.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
     'clearCacheOnLoad' => 1,
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-6.99.99',
+            'typo3' => '6.2.0-7.99.99',
             'filemetadata' => '',
         ),
         'conflicts' => array(),
