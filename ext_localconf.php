5d37523a1669917130344b5e8483fba858d97636
Pierrick Caillon <pcaillon@plan-net.fr>
2016-09-20 12:53:32 +0200

    [BUGFIX] Disable LanguageExtractor when using SolrCell (resolves #40)

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,7 +5,11 @@ if (!defined('TYPO3_MODE')) {
 
 $metaDataExtractorRegistry = \TYPO3\CMS\Core\Resource\Index\ExtractorRegistry::getInstance();
 $metaDataExtractorRegistry->registerExtractionService('ApacheSolrForTypo3\\Tika\\Service\\Extractor\\MetaDataExtractor');
-$metaDataExtractorRegistry->registerExtractionService('ApacheSolrForTypo3\\Tika\\Service\\Extractor\\LanguageDetector');
+$extConf = unserialize($_EXTCONF);
+if ($extConf['extractor'] !== 'solr') {
+    $metaDataExtractorRegistry->registerExtractionService('ApacheSolrForTypo3\\Tika\\Service\\Extractor\\LanguageDetector');
+}
+unset($extConf);
 
 if (version_compare(TYPO3_version, '7.1', '>')) {
     $textExtractorRegistry = \TYPO3\CMS\Core\Resource\TextExtraction\TextExtractorRegistry::getInstance();
