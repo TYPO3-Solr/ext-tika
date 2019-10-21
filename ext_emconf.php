c07a917e50743e371a9121e216efaecde151566f
Timo Hund <timo.hund@yahoo.com>
2019-10-21 10:41:04 +0200

    5.0.0 - Maintenance release
    This release:
    
    * Add's compatibility for EXT:solr 10
    
    Please read the release notes before updating:
    
    https://github.com/TYPO3-Solr/ext-tika/releases/tag/5.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => 1,
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-',
+            'typo3' => '9.5.0-9.5.99',
             'filemetadata' => '',
         ],
         'conflicts' => [],
