209041642032752dbcc0ea12926f3221581f6363
Rafael Kähm <rafael.kaehm@dkd.de>
2020-05-15 13:45:39 +0200

    [TASK] Make compatible with TYPO3 10.4  ::: fix dependencies

--- ext_tables.php
+++ ext_tables.php
@@ -1,13 +1,13 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+// Prevent Script from beeing called directly
+defined('TYPO3_MODE') || die();
 
 if (TYPO3_MODE == 'BE') {
     // adding the Solr connection status to the status report
     $statusSection = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('solr') ? 'solr' : 'tika';
 
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers'][$statusSection][] = \ApacheSolrForTypo3\Tika\Report\TikaStatus::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers'][$statusSection][] =
+        \ApacheSolrForTypo3\Tika\Report\TikaStatus::class;
     $extIconPath = 'EXT:tika/Resources/Public/Images/Icons/';
 
 
