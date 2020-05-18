a49ddabc00f3fbcff5c29c8ea0cecf455927f879
Rafael Kähm <rafael.kaehm@dkd.de>
2020-05-18 23:35:24 +0200

    [TASK] Make compatible with TYPO3 10.4  ::: fix deprecations
    * removes TYPO3 < 8 conditional calls
    * removes $_EXTKEY
    * removes `if (TYPO3_MODE === 'BE')` in conf files

--- ext_tables.php
+++ ext_tables.php
@@ -2,20 +2,19 @@
 // Prevent Script from beeing called directly
 defined('TYPO3_MODE') || die();
 
-if (TYPO3_MODE == 'BE') {
-    // adding the Solr connection status to the status report
-    $statusSection = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('solr') ? 'solr' : 'tika';
+// adding the Solr connection status to the status report
+$statusSection = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('solr') ? 'solr' : 'tika';
 
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers'][$statusSection][] =
-        \ApacheSolrForTypo3\Tika\Report\TikaStatus::class;
-    $extIconPath = 'EXT:tika/Resources/Public/Images/Icons/';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers'][$statusSection][] =
+    \ApacheSolrForTypo3\Tika\Report\TikaStatus::class;
+$extIconPath = 'EXT:tika/Resources/Public/Images/Icons/';
 
 
-    $modulePrefix = 'extensions-tika-module';
-    $svgProvider = \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class;
+$modulePrefix = 'extensions-tika-module';
+$svgProvider = \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class;
 
-    /* @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */ // register all module icons with extensions-solr-module-modulename
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+/* @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */ // register all module icons with extensions-solr-module-modulename
+$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
 
     $iconRegistry->registerIcon(
         $modulePrefix . '-tikacontrolpanel',
@@ -25,28 +24,27 @@ if (TYPO3_MODE == 'BE') {
 
 
 
-    if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('solr')) {
-        $tikaExtensionConfiguration = \ApacheSolrForTypo3\Tika\Util::getTikaExtensionConfiguration();
-        $isSolrModuleEnabled = (is_array($tikaExtensionConfiguration)
-            && isset($tikaExtensionConfiguration['showTikaSolrModule'])
-            && $tikaExtensionConfiguration['showTikaSolrModule'] == 1);
+if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('solr')) {
+    $tikaExtensionConfiguration = \ApacheSolrForTypo3\Tika\Util::getTikaExtensionConfiguration();
+    $isSolrModuleEnabled = (is_array($tikaExtensionConfiguration)
+        && isset($tikaExtensionConfiguration['showTikaSolrModule'])
+        && $tikaExtensionConfiguration['showTikaSolrModule'] == 1);
 
-        if ($isSolrModuleEnabled) {
-            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-                'ApacheSolrForTypo3.' . $_EXTKEY,
-                'searchbackend',
-                'TikaControlPanel',
-                'bottom',
-                [
-                    'Backend\\SolrModule\\TikaControlPanelModule' => 'index, startServer, stopServer'
-                ],
-                [
-                    'access' => 'user,group',
-                    'icon' => 'EXT:' . $_EXTKEY . '/Resources/Public/Images/Icons/module-tika.svg',
-                    'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xlf:solr.backend.tika.label'
-                ]
-            );
-        }
-
+    if ($isSolrModuleEnabled) {
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+            'ApacheSolrForTypo3.tika',
+            'searchbackend',
+            'TikaControlPanel',
+            'bottom',
+            [
+                'Backend\\SolrModule\\TikaControlPanelModule' => 'index, startServer, stopServer'
+            ],
+            [
+                'access' => 'user,group',
+                'icon' => 'EXT:tika/Resources/Public/Images/Icons/module-tika.svg',
+                'labels' => 'LLL:EXT:tika/Resources/Private/Language/locallang.xlf:solr.backend.tika.label'
+            ]
+        );
     }
+
 }
