268aca8d1a2ee7f06f45bcc0c43d9f357ff422cc
Lars Tode <lars.tode@dkd.de>
2021-03-12 09:58:27 +0100

    [TASK] Download of Apache Tika
    Change download URL of Apache Tika app and server

--- Build/Test/bootstrap.sh
+++ Build/Test/bootstrap.sh
@@ -2,6 +2,7 @@
 
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 EXTENSION_ROOTPATH="$SCRIPTPATH/../../"
+TIKA_SERVER_SOURCE="https://archive.apache.org/dist/tika/"
 
 #
 # Run this script once to set up a dev/test environment for this extension
@@ -9,41 +10,41 @@ EXTENSION_ROOTPATH="$SCRIPTPATH/../../"
 #
 
 if [[ $* == *--local* ]]; then
-  echo -n "Choose a TYPO3 Version (e.g. dev-master,~6.2.17,~7.6.2): "
-  read typo3Version
-  export TYPO3_VERSION=$typo3Version
+    echo -n "Choose a TYPO3 Version (e.g. dev-master, ~6.2.17,~7.6.2): "
+    read typo3Version
+    export TYPO3_VERSION=$typo3Version
 
-  echo -n "Choose a EXT:solr Version (e.g. dev-master,~3.1.1): "
-  read extSolrVersion
-  export EXT_SOLR_VERSION=$extSolrVersion
+    echo -n "Choose a EXT:solr Version (e.g. dev-master, dev-master,~3.1.1): "
+    read extSolrVersion
+    export EXT_SOLR_VERSION=$extSolrVersion
 
-  echo -n "Choose a tika Version (e.g. 1.15): "
-  read tikaVersion
-  export TIKA_VERSION=$tikaVersion
+    echo -n "Choose a tika Version (e.g. 1.15): "
+    read tikaVersion
+    export TIKA_VERSION=$tikaVersion
 
-  echo -n "Choose a database hostname: "
-  read typo3DbHost
-  export TYPO3_DATABASE_HOST=$typo3DbHost
+    echo -n "Choose a database hostname: "
+    read typo3DbHost
+    export TYPO3_DATABASE_HOST=$typo3DbHost
 
-  echo -n "Choose a database name: "
-  read typo3DbName
-  export TYPO3_DATABASE_NAME=$typo3DbName
+    echo -n "Choose a database name: "
+    read typo3DbName
+    export TYPO3_DATABASE_NAME=$typo3DbName
 
-  echo -n "Choose a database user: "
-  read typo3DbUser
-  export TYPO3_DATABASE_USERNAME=$typo3DbUser
+    echo -n "Choose a database user: "
+    read typo3DbUser
+    export TYPO3_DATABASE_USERNAME=$typo3DbUser
 
-  echo -n "Choose a database password: "
-  read typo3DbPassword
-  export TYPO3_DATABASE_PASSWORD=$typo3DbPassword
+    echo -n "Choose a database password: "
+    read typo3DbPassword
+    export TYPO3_DATABASE_PASSWORD=$typo3DbPassword
 fi
 
 test -n "$TIKA_PATH" || TIKA_PATH="$HOME/bin"
 
 
 if [ -z $TIKA_VERSION ]; then
-  echo "Must set env var TIKA_VERSION"
-  exit 1
+	echo "Must set env var TIKA_VERSION"
+	exit 1
 fi
 
 if [ -z $EXT_SOLR_VERSION ]; then
@@ -52,14 +53,14 @@ if [ -z $EXT_SOLR_VERSION ]; then
 fi
 
 if [ -z $TYPO3_VERSION ]; then
-  echo "Must set env var TYPO3_VERSION"
-  exit 1
+	echo "Must set env var TYPO3_VERSION"
+	exit 1
 fi
 
 wget --version > /dev/null 2>&1
 if [ $? -ne "0" ]; then
-  echo "Couldn't find wget."
-  exit 1
+	echo "Couldn't find wget."
+	exit 1
 fi
 
 if ! java -version 2>&1 >/dev/null ; then
@@ -69,24 +70,28 @@ fi
 
 # download Tika if not present
 if [ ! -d "$TIKA_PATH" ]; then
-  mkdir -p "$TIKA_PATH"
+	mkdir -p "$TIKA_PATH"
 fi
+
 if [ ! -f "$TIKA_PATH/tika-app-$TIKA_VERSION.jar" ]; then
-  wget "https://archive.apache.org/dist/tika/tika-app-$TIKA_VERSION.jar" -O "$TIKA_PATH/tika-app-$TIKA_VERSION.jar"
+	wget "${TIKA_SERVER_SOURCE}tika-app-$TIKA_VERSION.jar" -O "$TIKA_PATH/tika-app-$TIKA_VERSION.jar"
+	test -f "$TIKA_PATH/tika-app-$TIKA_VERSION.jar" || echo "Could not download tika-app-$TIKA_VERSION.jar from ${TIKA_SERVER_SOURCE}" && exit 1
 else
-  echo "Cached $TIKA_PATH/tika-app-$TIKA_VERSION.jar present"
+	echo "Cached $TIKA_PATH/tika-app-$TIKA_VERSION.jar present"
 fi
+
 if [ ! -f "$TIKA_PATH/tika-server-$TIKA_VERSION.jar" ]; then
-  wget "https://archive.apache.org/dist/tika/tika-server-$TIKA_VERSION.jar" -O "$TIKA_PATH/tika-server-$TIKA_VERSION.jar"
+	wget "${TIKA_SERVER_SOURCE}tika-server-$TIKA_VERSION.jar" -O "$TIKA_PATH/tika-server-$TIKA_VERSION.jar"
+	test -f "$TIKA_PATH/tika-server-$TIKA_VERSION.jar" || echo "Could not download tika-server-$TIKA_VERSION.jar from ${TIKA_SERVER_SOURCE}" && exit 1
 else
-  echo "Cached $TIKA_PATH/tika-server-$TIKA_VERSION.jar present"
+	echo "Cached $TIKA_PATH/tika-server-$TIKA_VERSION.jar present"
 fi
 
 # stop Tika server if one is still running
 if [ -f ./tika_pid ]; then
-  TIKA_PID=cat ./tika_pid
-  echo "Stopping Tika ($TIKA_PID)"
-  kill $TIKA_PID
+	TIKA_PID=cat ./tika_pid
+	echo "Stopping Tika ($TIKA_PID)"
+	kill $TIKA_PID
 fi
 
 # start tika server
@@ -122,5 +127,7 @@ export TYPO3_PATH_WEB=$PWD/.Build/Web
 mkdir -p $TYPO3_PATH_WEB/uploads $TYPO3_PATH_WEB/typo3temp
 
 # Setup Solr using install script
-chmod u+x ${TYPO3_PATH_WEB}/typo3conf/ext/solr/Resources/Private/Install/install-solr.sh
-${TYPO3_PATH_WEB}/typo3conf/ext/solr/Resources/Private/Install/install-solr.sh -d "$HOME/solr" -t
+if [[ $* != *--skip-solr-install* ]]; then
+    chmod u+x ${TYPO3_PATH_WEB}/typo3conf/ext/solr/Resources/Private/Install/install-solr.sh
+    ${TYPO3_PATH_WEB}/typo3conf/ext/solr/Resources/Private/Install/install-solr.sh -d "$HOME/solr" -t
+fi
