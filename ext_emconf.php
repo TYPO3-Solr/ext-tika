95e41ed7718e2aa99b876dfed8614c28f06423d6
Rafael Kähm <rafael.kaehm@dkd.de>
2021-03-12 14:19:01 +0100

    [TASK] Prepare Release 6.1
    * Removes TYPO3 9.5 from compatibility list and travis build stack
    * Removes suggestion to install EXT:devlog, since it is outdated.
    * Removes Dockerfile, since official docker image for Tika exists
      and no changes for are required
    * Sets master branch as alias for 6.1.x-dev
    
    Fixes: #141
    Fixes: #148

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Apache Tika for TYPO3',
     'description' => 'Provides Tika services for TYPO3 to detect a document\'s language, extract meta data, and extract content from files. Can either use a stand alone Tika executable or Tika integrated in a Solr server with an activated extracting request handler.',
-    'version' => '6.0.0',
+    'version' => '6.1.0',
     'state' => 'stable',
     'category' => 'services',
     'author' => 'Ingo Renner, Timo Hund, Markus Friedrich',
@@ -15,13 +15,12 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => 1,
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.16-10.4.99',
+            'typo3' => '10.4.10-10.4.99',
             'filemetadata' => ''
         ],
         'conflicts' => [],
         'suggests' => [
-            'solr' => '11.0.0-',
-            'devlog' => '',
+            'solr' => '11.1.0-0.0.0'
         ],
     ],
     '_md5_values_when_last_written' => ''
