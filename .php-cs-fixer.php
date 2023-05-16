50757e662a8297670d9d93201f7aa7fe16e02926
Rafael Kähm <rafael.kaehm@dkd.de>
2023-05-16 18:13:43 +0200

    [TASK] Fix TYPO3 coding standards differences between PHP versions
    This change contains: 
    
    * The PSR12 rule for import type blocks for PhpcsFixer 
       The ordered_imports.imports_order is now 
       class, function, const
    * the coding standards fixes for TYPO3 12+ as well.

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -3,7 +3,16 @@
 declare(strict_types=1);
 
 $config = \TYPO3\CodingStandards\CsFixerConfig::create();
-$config->getFinder()
+$config
+    ->addRules(
+        [
+            'ordered_imports' => [
+                'imports_order' => ['class', 'function', 'const'],
+                'sort_algorithm' => 'alpha'
+            ]
+        ],
+    )
+    ->getFinder()
     ->exclude([
         '.Build'
     ])
