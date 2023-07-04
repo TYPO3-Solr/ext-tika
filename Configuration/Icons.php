0fe8b6de16482c5f8a38e6faadff9fa78f6388e6
Georg Ringer <georg.ringer@gmail.com>
2023-07-04 08:55:34 +0200

    [TASK] Migrate icon registration

---
+++ Configuration/Icons.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'extensions-tika-module-tikacontrolpanel' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:solr/Resources/Public/Images/Icons/module-tika.svg',
+    ],
+
+];
