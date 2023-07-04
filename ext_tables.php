0be4ac5c44f9274a966c2455ee10b87b6f6ad359
Georg Ringer <georg.ringer@gmail.com>
2023-07-04 08:55:34 +0200

    [TASK] Migrate icon registration

--- ext_tables.php
+++ ext_tables.php
@@ -7,19 +7,6 @@ $statusSection = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('s
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers'][$statusSection][] =
     \ApacheSolrForTypo3\Tika\Report\TikaStatus::class;
-$extIconPath = 'EXT:tika/Resources/Public/Images/Icons/';
-
-$modulePrefix = 'extensions-tika-module';
-$svgProvider = \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class;
-
-/* @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */ // register all module icons with extensions-solr-module-modulename
-$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-
-$iconRegistry->registerIcon(
-    $modulePrefix . '-tikacontrolpanel',
-    $svgProvider,
-    ['source' => $extIconPath . 'module-tika.svg']
-);
 
 if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('solr')) {
     $tikaExtensionConfiguration = \ApacheSolrForTypo3\Tika\Util::getTikaExtensionConfiguration();
