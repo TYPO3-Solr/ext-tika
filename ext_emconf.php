d616ecb09f3c1993069b9b4de0d4997cf5201f81
Rafael Kähm <rafael.kaehm@dkd.de>
2025-12-18 14:46:43 +0100

    [TASK] Prepare release-12.1.x branch
    New major release branch for EXT:tika 12.1.0+ releases.

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Apache Tika for TYPO3',
     'description' => 'Provides Tika services for TYPO3 to detect a document\'s language, extract meta data, and extract content from files. Can either use a stand alone Tika executable or Tika integrated in a Solr server with an activated extracting request handler.',
-    'version' => '12.0.4',
+    'version' => '12.1.0',
     'state' => 'stable',
     'category' => 'services',
     'author' => 'Ingo Renner, Timo Hund, Markus Friedrich, Rafael Kähm',
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = [
         ],
         'conflicts' => [],
         'suggests' => [
-            'solr' => '12.0.4-',
+            'solr' => '12.1.0-',
         ],
     ],
     'autoload' => [
